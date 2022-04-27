9724fe03cc9817dbfdf6305b39601a4ee39717b1
Tom5565 <37259081+Tom5565@users.noreply.github.com>
2022-04-28 05:34:27 +0900

    update to Selenium 4

--- scrape.py
+++ scrape.py
@@ -3,27 +3,27 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
 import pandas as pd
 
 import login_config #import credentials
 
 
-
+# Chrome settings
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument("--incognito") #don't want cache
 
 service = Service(ChromeDriverManager().install()) 
-    
 driver = webdriver.Chrome(service, options= chrome_options)
 url = "https://campus.icu.ac.jp/icumap/ehb/SearchCO.aspx"
 
-# Open Site (will be sent to SSO login)
+# Open site (will be sent to SSO login)
 driver.get(url)
 driver.implicitly_wait(10)
 
 # Login to ICU SSO
-driver.find_element_by_id("username_input").send_keys(login_config.username)
-driver.find_element_by_id ("password_input").send_keys(login_config.password)
-driver.find_element_by_id("login_button").click()
+driver.find_element(By.CLASS_NAME,"username_input").send_keys(login_config.username)
+driver.find_element(By.CLASS_NAME,"password_input").send_keys(login_config.password)
+driver.find_element(By.CLASS_NAME,"login_button").click()
 
