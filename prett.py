52e9abeb7a0a747ea6c4d4249cf5fb78d479b5a6
Tom5565 <37259081+Tom5565@users.noreply.github.com>
2022-04-28 07:49:04 +0900

    cleanup

--- prett.py
+++ prett.py
@@ -4,15 +4,14 @@ import pandas as pd
 
 table = scrape.get_courses()
 
-df = pd.read_html(str(table))
-print(df[0].to_json(orient='records'))
+#df = pd.read_html(str(table))
+#print(df[0].to_json(orient='records'))
 
 
 
-###soup = BeautifulSoup(html,'html.parser')
-###courses = soup.find('tr').find_all('tr')
-###
-###for i in range(3):
-###    print(courses[i])
-###    print('----------')
-###
+soup = BeautifulSoup(table,'lxml')
+courses = soup.find('tr').find_all('tr')
+
+for i in range(3):
+    print(courses[i])
+    print('----------')
