d0706a6e99bd632beb24afdc5dc843de01cf584a
Tom5565 <37259081+Tom5565@users.noreply.github.com>
2022-04-28 06:40:47 +0900

    finalised scrape function

--- scrape.py
+++ scrape.py
@@ -1,10 +1,10 @@
+import traceback
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.by import By
-import pandas as pd
 
 import login_config #import credentials
 
@@ -14,20 +14,35 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument("--incognito") #don't want cache
 
-service = Service(ChromeDriverManager().install()) 
-driver = webdriver.Chrome(service=service, options= chrome_options)
-url = "https://campus.icu.ac.jp/icumap/ehb/SearchCO.aspx"
+def get_courses():
+    try: 
+        service = Service(ChromeDriverManager().install()) 
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        url = "https://campus.icu.ac.jp/icumap/ehb/SearchCO.aspx"
 
-# Open site (will be sent to SSO login)
-driver.get(url)
-driver.implicitly_wait(3)
+        # Open site (will be sent to SSO login)
+        driver.get(url)
+        driver.implicitly_wait(3)
 
-# Login to ICU SSO
-driver.find_element(By.ID,"username_input").send_keys(login_config.username)
-driver.find_element(By.ID,"password_input").send_keys(login_config.password)
-driver.find_element(By.ID,"login_button").click()
-driver.implicitly_wait(3)
+        # Login to ICU SSO
+        driver.find_element(By.ID,"username_input").send_keys(login_config.username)
+        driver.find_element(By.ID,"password_input").send_keys(login_config.password)
+        driver.find_element(By.ID,"login_button").click()
+        driver.implicitly_wait(3)
 
-select_element = driver.find_element(By.ID,"ctl00_ContentPlaceHolder1_ddlPageSize")
-select_object = Select(select_element)
-select_object.select_by_visible_text("ALL")
+        # Select show ALL results to get full course list
+        select_element = driver.find_element(By.ID,"ctl00_ContentPlaceHolder1_ddlPageSize")
+        select_object = Select(select_element)
+        select_object.select_by_visible_text("ALL")
+        driver.find_element(By.ID,"ctl00_ContentPlaceHolder1_btn_search").click()
+        driver.implicitly_wait(3)
+
+        # Find course table
+        tables = driver.find_elements(By.TAG_NAME,"tbody")
+        course_table = tables[3].get_attribute('innerHTML')
+
+        return course_table
+    except:
+        traceback.print_exc()
+    finally:
+        driver.quit()
