17a0dee649c79754b7c5c9819cd566708bacb138
Pentacoxian <c241494r@icu.ac.jp>
2022-06-02 10:29:05 +0900

    add getSyllabus

--- scrape.py
+++ scrape.py
@@ -13,6 +13,9 @@ import login_config #import credentials
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrome_options.add_argument("--incognito") #don't want cache
+chrome_options.add_argument("--headless")
+
+
 
 def get_courses():
     try: 
@@ -40,9 +43,52 @@ def get_courses():
         # Find course table
         tables = driver.find_elements(By.TAG_NAME,"table")
         course_table = tables[3].get_attribute('innerHTML')
-
+        #print(course_table)
         return course_table
     except:
         traceback.print_exc()
     finally:
         driver.quit()
+
+
+def getSyllabus(year,regno):
+    try:
+        #https://campus.icu.ac.jp/icumap/ehb/PreviewSyllabus.aspx?year=2022&term=1&regno=10101
+        service = Service(ChromeDriverManager().install()) 
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+        # Init SSO
+        url = "https://campus.icu.ac.jp/icumap/ehb/SearchCO.aspx"
+        # Open site (will be sent to SSO login)
+        driver.get(url)
+        driver.implicitly_wait(0.5)
+        # Login to ICU SSO
+        driver.find_element(By.ID,"username_input").send_keys(login_config.username)
+        driver.find_element(By.ID,"password_input").send_keys(login_config.password)
+        driver.find_element(By.ID,"login_button").click()
+        driver.implicitly_wait(0.5)
+
+        resHTML = ""
+        for i in range(len(regno)):
+            print(regno[i])
+            url = "https://campus.icu.ac.jp/public/ehandbook/PreviewSyllabus.aspx?year="+year+"&regno="+regno[i]+"&term="+regno[0][0]
+            print(url)
+            # Open site
+            driver.get(url)
+            driver.implicitly_wait(3)
+            # Find course table
+            tables = driver.find_elements(By.TAG_NAME,"table")
+            contents = tables[4].get_attribute('innerHTML')
+            print(contents)
+        #print(course_table)
+        return resHTML
+    except:
+        traceback.print_exc()
+    finally:
+        driver.quit()
+
+#get_courses()
+
+# Debug
+#f = open("./log.txt","x")
+#f.write(get_courses())
+#f.close()
---
+++ testg.py
@@ -0,0 +1,7 @@
+import scrape
+
+testRegno = ["32410","32411","32412","32501","32502","32503","32504"]
+#for i in range(len(testRegno)):
+#    scrape.getSyllabus("2022",testRegno[i])
+
+scrape.getSyllabus("2022",testRegno)
