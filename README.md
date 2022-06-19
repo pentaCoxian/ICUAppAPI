b130cb994c00787650268472ee67b656c5a97d3b
Pentacoxian <c241494r@icu.ac.jp>
2022-06-19 23:43:02 +0900

    cleanup

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # ICUSyllabusScrape
 
-aspxのシラバスをどうにかしてスクレイピングしようとしてみるやーつ
+ICUのシラバスとコースオファリングスのスクレイピングを行ってmongodb atlasに挿入するスクリプトです。
 
-## dep
+## 必要パッケージ
 - selenium
 - webdriver-manager
 - beautifulsoup4
 - lxml
 - pymongo
+- dnspython
 - certifi
-- tqdm (for dev)
-かな？
+- tqdm (進捗確認用、必要なければ`scrape.py`のl:84`for i in tqdm(range(len(regno))):`からtqdmを外す)
+かな？エラー出たら適時インストールしてください…
 
-## TODO
-[] make schedule an array
+## Useage
+
+`mongoAccess.py`のCONNECTION_STRINGを作成したmongodb atlasの設定に従って作成してください。その後`login_config_example.py`を`login_config.py`に名称変更して内容をicu portalのログイン情報に変更したら実行できるはずです。
--- mongoAccess.py
+++ mongoAccess.py
@@ -9,23 +9,16 @@ def get_database():
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     CONNECTION_STRING = "mongodb+srv://python:lolpython556@pythondev.etdrtaj.mongodb.net/pythondev"
-
-    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-    from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
-
-    # Create the database for our example (we will use the same database throughout the tutorial
     return client['icu']
     
-# This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":    
     
 
     # Get the database
     dbname = get_database()
-
     collection_name = dbname["courseOfferings"]
-    #collection_name.insert_many(yakeluso.getCourseInfo())
+    collection_name.insert_many(yakeluso.getCourseInfo())
 
 
     regIdList = []
