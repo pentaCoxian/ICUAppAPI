247f455ca284347728445dcd352e6696026b9056
pentacoxian <c241494r@icu.ac.jp>
2022-07-17 08:25:26 +0000

    add url query

---
+++ dbAccess/search.py
@@ -0,0 +1,150 @@
+import MeCab as mb
+import os
+import scrape
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import inspect
+from sqlalchemy import select
+from sqlalchemy import desc
+from sqlalchemy.dialects.mysql import match
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import time
+#import scrape
+load_dotenv()
+engine = sa.create_engine(os.environ['MARIADB_ADDRESS'],echo=False)
+Base = declarative_base()
+
+# msg = '''
+# このコースはNOHA事務局とICU間で締結された協定に基づいて提供されるものである。本コースの主要な目的は、自然災害の分野における人道アクションに焦点を当て、特に災害リスク減少に関連する日本の政策と実践を理解することである。学術界、政府、市民社会などにおいて災害への対応、リスク減少などに取り組んでいる専門家を講師として招き、オムニバス形式で開講する。
+# '''
+# print('INPUT: ',msg)
+# tagger = mb.Tagger('-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')#sudo find / | grep mecab-ipadic-neologd
+# #tagger = mb.Tagger('-d /usr/share/mecab/dic/ipadic -O chasen')
+# #tagger = mb.Tagger()
+# start=time.time()
+# rs = tagger.parseToNode(msg)
+# def ncomp(rs):
+#     r = []
+#     while rs:
+#         hold = rs.feature.split(',')
+#         type = hold[0]
+#         subtype = hold[1]
+#         dia1 = hold[6]
+#         word = rs.surface
+#         #print(word,rs.feature)
+#         if type == '名詞':
+#             #print(word,type,subtype,dia1)
+#             r.append(word)
+#         rs = rs.next
+#     t='+ '.join(r)
+#     #print(t)
+#     return t
+# def comp(rs):
+#     r = set()
+#     while rs:
+#         hold = rs.feature.split(',')
+#         type = hold[0]
+#         subtype = hold[1]
+#         dia1 = hold[6]
+#         word = rs.surface
+#         #print(word,rs.feature)
+#         if type == '名詞':
+#             #print(word,type,subtype,dia1)
+#             r.add(word)
+#         rs = rs.next
+#     t=' +'.join(r)
+#     print('TAGS: ',t)
+#     return t
+# print('size difference:',len(msg),'->',len(comp(rs)))
+# print("--- %s seconds ---" % (time.time() - start))
+
+
+class Course(Base):
+    __tablename__ ="syllabuses"
+    __table_args__ = {
+        'mariadb_ENGINE': 'mroonga',
+        'mariadb_DEFAULT_CHARSET': 'utf8mb4'
+    }
+    regno = sa.Column(sa.Integer, primary_key=True)
+    ay = sa.Column(sa.String(length=5))
+    term = sa.Column(sa.String(length=100))
+    cno = sa.Column(sa.String(length=100))
+    title_e = sa.Column(sa.String(length=300))
+    title_j = sa.Column(sa.String(length=300))
+    lang = sa.Column(sa.String(length=300))
+    instructor = sa.Column(sa.String(length=100))
+    unit_e = sa.Column(sa.String(length=100))
+    koma_lecture_e = sa.Column(sa.String(length=10))
+    koma_seminar_e = sa.Column(sa.String(length=10))
+    koma_labo_e = sa.Column(sa.String(length=10))
+    koma_act_e = sa.Column(sa.String(length=10))
+    koma_int_e = sa.Column(sa.String(length=10))
+    descreption = sa.Column(sa.Text)
+    descreption_j = sa.Column(sa.Text)
+    goals = sa.Column(sa.Text)
+    goals_j = sa.Column(sa.Text)
+    content = sa.Column(sa.Text)
+    content_j = sa.Column(sa.Text)
+    lang_of_inst = sa.Column(sa.Text)
+    pollicy = sa.Column(sa.Text)
+    individual_study = sa.Column(sa.Text)
+    ref = sa.Column(sa.Text)
+    notes = sa.Column(sa.Text)
+    schedule = sa.Column(sa.String(length=500))
+    url = sa.Column(sa.String(length=300))
+
+Base.metadata.create_all(engine)
+Session = sa.orm.sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+def MakeIndex(engine, tablename, indexname, cols): 
+    cols = ','.join(cols)
+    with engine.connect() as con:
+        con.execute("""ALTER TABLE """ + tablename + """ ADD FULLTEXT INDEX IF NOT EXISTS """ + indexname +  """fulltextIndex2(""" + cols + """) COMMENT 'tokenizer "TokenMecab"';""")
+
+
+MakeIndex(engine,'syllabuses','fulltextindex1',['content_j'])
+MakeIndex(engine,'syllabuses','fulltextindex2',['lang_of_inst'])
+MakeIndex(engine,'syllabuses','fulltextindex7',['schedule'])
+MakeIndex(engine,'syllabuses','fulltextindex5',['cno','schedule'])
+MakeIndex(engine,'syllabuses','fulltextindex3',['content','content_j'])
+MakeIndex(engine,'syllabuses','fulltextindex4',['descreption','descreption_j'])
+MakeIndex(engine,'syllabuses','fulltextindex6',['ay', 'term', 'cno', 'title_e', 'title_j', 'lang', 'instructor', 'unit_e', 'koma_lecture_e', 'koma_seminar_e', 'koma_labo_e', 'koma_act_e', 'koma_int_e', 'descreption', 'descreption_j', 'goals', 'goals_j', 'content', 'content_j', 'lang_of_inst', 'pollicy', 'individual_study', 'ref', 'notes', 'schedule', 'url'])
+
+
+
+#start=time.time()
+## courses = session.query(Course).all()
+## for course in courses:
+##     print(" - " + course.title_j + ' ' + course.title_e)
+#pron=' +ISC +5 +6 +7'
+#vars='ay, term, cno, title_e, title_j, lang, instructor, unit_e, koma_lecture_e, koma_seminar_e, koma_labo_e, koma_act_e, koma_int_e, descreption, descreption_j, goals, goals_j, content, content_j, lang_of_inst, pollicy, individual_study, ref, notes, schedule, url'
+#sql = """select * from syllabuses where match("""+vars+""") against('"""+pron+"""' in boolean mode);"""
+#res = session.execute(sql)
+#print('SEARCH:',pron)
+#for v in res:
+#   print(v.cno,' : ',v.title_j, v.schedule.strip('\n'))
+#print("--- %s seconds ---" % (time.time() - start))
+
+
+def searchFullText(arg,feild='ay, term, cno, title_e, title_j, lang, instructor, unit_e, koma_lecture_e, koma_seminar_e, koma_labo_e, koma_act_e, koma_int_e, descreption, descreption_j, goals, goals_j, content, content_j, lang_of_inst, pollicy, individual_study, ref, notes, schedule, url'):
+    pron=arg
+    vars=feild
+    sql = """select * from syllabuses where match("""+vars+""") 
+    against('"""+pron+"""' in boolean mode);"""
+    res = session.execute(sql)
+    print('SEARCH:',pron)
+    drers = []
+    for v in res:
+        print(v.regno,' : ',v.cno,v.title_j, v.schedule.strip('\n'),v.unit_e)
+
+        cdict = {}
+        cdict['regno']=v.regno
+        
+        drers.append(cdict)
+    return drers
+
+
--- falconCode/sdata.py
+++ falconCode/sdata.py
@@ -1,6 +1,10 @@
 import json
 import falcon
 
+import sys
+sys.path.append("../dbAccess")
+from search import searchFullText
+
 class Sdata:
     def on_get(self, req, resp):
         doc = {
@@ -8,3 +12,13 @@ class Sdata:
             'obj': str(req)
         }
         resp.text = json.dumps(doc, ensure_ascii=False)
+
+class GetData:
+    def on_get(self,req,resp):
+        q=''
+        for k,v in req.params.items():
+            q += ' +'+v
+        print(q)
+        f = searchFullText(q)
+        print(f)
+        resp.text = json.dumps(f, ensure_ascii=False)
